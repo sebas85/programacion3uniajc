8fdd08aaeb7c0c23e31cf2b418d771f31704544c
sebas85 <fsgarcia@estudiante.uniajc.edu.co>
2025-02-11 17:22:06 +0000

    Se agrego llenar arreglo aleatorio

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -9,8 +9,12 @@ public class Ejercicio1 {
 
             // Crear un arreglo de enteros
             int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            
+            
             // Mostrar los elementos del arreglo
            System.out.println(op.mostrarElementos(a));
-           system.out.printl("La suma de los elementos del arreglo es: +op.sumarElementos(a)")
+           System.out.println("La suma de los elementos del arreglo es: " + op.sumarElementos(a));
+           System.out.println("El promedio de los elementos del arreglo es: " + op.promedioElementos(a));
+       }
     }
-}
+
