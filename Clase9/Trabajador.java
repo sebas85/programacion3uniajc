fa566c88f3878359df91c237242c3ab4b7f05142
sebas85 <fsgarcia@estudiante.uniajc.edu.co>
2025-04-01 16:12:11 +0000

    Creacion de la carpeta clase 9 y la clase  trabajador

---
+++ Clase9/Trabajador.java
@@ -0,0 +1,18 @@
+package Clase9;
+
+
+public class Trabajador{
+
+
+    private String cedula;
+    private String nombre;
+    private double salario;   
+    
+    public Trabajador(String cedula, String nombre, double salario){
+        this.cedula = 
+    }
+
+    
+ 
+}
+
